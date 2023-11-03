5b123f27b88f5179d12ea989cff0f6b53027799d
Matthew Garry <mhgarry92@gmail.com>
2023-11-02 22:52:11 -0400

    define init variables

---
+++ main.py
@@ -0,0 +1,8 @@
+import secrets # import secrets module for generating random characters
+import string # import string module for string operations
+
+# variables for letters, digits, and special characters to be used in later generated passwords 
+letters = string.ascii_letters # generates all lowercase and uppercase letters in the alphabet
+digits = string.digits # generates all digits from 0-9
+special_chars = string.punctuation # generates all special characters we are using in this program
+
